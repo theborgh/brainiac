38ae7e7bc82391f44f9c6e363b32848e1dca9307
theborgh <borghiborghi@gmail.com>
2019-03-05 20:16:54 +0100

    Fix logo

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Tilt from 'react-tilt';
+import brainlogo from './brainlogo.png'
+import './Logo.css';
 
 const Logo = () => {
    return (
       <div className='ma4 mt0'>
-
+         <Tilt className="Tilt br2 shadow-2" options={{ max : 45 }} style={{ height: 150, width: 150 }} >
+            <div className="Tilt-inner pa3 "><img style={{paddingTop: '5px'}} src={brainlogo} alt='logo' /></div>
+         </Tilt>
       </div>
    );
 }
